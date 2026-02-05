6702876d00d4b781ad086651f6dc4ad53fe69631
Hamzah Marie <mariehamzah1@gmail.com>
2026-02-05 13:24:30 -0500

    add proxy to protect routes / handle redirects if un-authorized or authorized

--- my-app/src/app/lib/supabase/server.ts
+++ my-app/src/app/lib/supabase/server.ts
@@ -6,7 +6,7 @@ export async function createServerSupabaseClient() {
 
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY!,
     {
       cookies: {
         getAll() {
@@ -15,8 +15,8 @@ export async function createServerSupabaseClient() {
         setAll(cookiesToSet) {
           try {
             cookiesToSet.forEach(({ name, value, options }) => cookieStore.set(name, value, options))
-          } catch (error){
-            console.log(error)
+          } catch {
+            
             // The `setAll` method was called from a Server Component.
             // This can be ignored if you have middleware refreshing
             // user sessions.
